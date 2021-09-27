ed162786e8935ccb846ac94952ccb12d5a1f1a89
jjcdesk <sjjkk1996>
2021-09-27 14:54:50 +0900

    0927 업로드 리드 까지 가능하게 구현한 코드

---
+++ src/main/java/org/zerock/mreview/dto/MovieDTO.java
@@ -0,0 +1,27 @@
+package org.zerock.mreview.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MovieDTO {
+  private Long mno;
+  private String title;
+  @Builder.Default
+  private List<MovieImageDTO> imageDTOList = new ArrayList<>();
+
+  private double avg;
+  private int reviewCnt;
+
+  private LocalDateTime regDate; //등록일자
+  private LocalDateTime modDate; //수정일자
+}
